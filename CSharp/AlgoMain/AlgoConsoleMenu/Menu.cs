eb9d3eb987debba8c7640b954d1cdd8cc5ebee02
Rahul Gautam <rahulbgautam@gmail.com>
2017-10-17 12:51:57 -0500

    repush as the earlier branch got lost

---
+++ CSharp/AlgoMain/AlgoConsoleMenu/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMenu
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Displaylabel { get; set; }
+        public int Level { get; set; }
+        public int ParentLevel { get; set; }
+
+
+        public List<Menu> AddMenuItem(Menu item, List<Menu> currentMenu)
+        {
+            currentMenu.Add(item);
+            return currentMenu;
+
+        }
+
+        public void DisplayMenu(List<Menu> menuitems) {
+            //TODO: Add logic to create Hierarchial menu sturcture (eg. Menu with subitems)
+
+
+        }
+    }
+}
