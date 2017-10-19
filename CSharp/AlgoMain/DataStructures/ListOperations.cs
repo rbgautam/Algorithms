0ebdc0fae707eba4f72efdddb6510e619f2489c0
Rahul Gautam <rahulbgautam@gmail.com>
2017-10-19 16:31:10 -0500

    Interface for CRUD operations

---
+++ CSharp/AlgoMain/DataStructures/ListOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Interface for the CRUD Operations
+    /// </summary>
+    public interface ListOperations<T>
+    {
+        bool Add();
+        T Fetch();
+        bool Update();
+        bool Delete();
+
+    }
+}
