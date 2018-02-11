6d9febdc51ce91a8a063961f9f8b2e05e65647e5
rahulbgautam <rahulbgautam@gmail.com>
2018-02-11 12:02:27 -0600

    Binary search in python

---
+++ Python/binsearch.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Binary Search
+
+This expects a sorted list of elements
+"""
+
+def binary_search(list, item):
+    low =0
+    high = len(list) -1
+    
+    while (low <= high):
+        mid = round((low + high)/2)
+        guess = list[mid]
+        
+        if(guess == item):
+            return mid
+        if guess >= item:
+            high = mid -1
+        else:
+            low = mid +1
+        return None       
+        
+    
+my_list = [1,2,5,8,9,13]
+
+print (binary_search(my_list,5))
+
+print (binary_search(my_list, -1))   
